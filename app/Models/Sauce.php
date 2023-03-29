97da4ac03513a561c4859a927f14db6fdf6b17c2
Alexandre Ferreira <ferreira64240@hotmail.com>
2023-03-29 18:17:20 +0200

    Création du projet laravel et du modele sauce

---
+++ app/Models/Sauce.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sauce extends Model
+{
+    use HasFactory;
+
+    protected $primaryKey = 'idSauce';
+    protected $fillable = ['idSauce','userId', 'name', 'manufacturer', 'description', 'mainPepper', 'imageUrl', 'heat', 'likes', 'dislikes', 'usersLiked', 'usersDisliked'];
+
+    public function user()
+        {
+        return $this->belongsTo('App\User');
+        }
+}
